b838b5c649c4a9a31bc666dfd6dad2ce2eb3f421
Govind Patel <govind.patel@hostbooks.com>
2024-06-03 08:03:47 +0530

    top loading and scrollbar

--- src/App.js
+++ src/App.js
@@ -6,30 +6,42 @@ import NavBar from './components/NavBar';
 import News from './components/News';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
   pageSize = 9;
+  state = {
+    progress:0
+  }
+  setProgress = (progress)=>{
+    this.setState({progress:progress})
+  }
   render() {
     return (
         <div>
           <Router>
             <NavBar />
+            <LoadingBar
+              color='#f11946'
+              progress={this.state.progress}
+              
+            />
             <Routes>
               {/* <Route path="/" element={<NavBar />} /> */}
-              <Route path="/" element={<News  pageSize={this.pageSize} key="general" country={'in'} category={'general'}/>} />
-              <Route path="/business" element={<News  pageSize={this.pageSize} key="business" country={'in'} category={'business'}/>} />
-              <Route path="/entertainment" element={<News pageSize={this.pageSize} key="entertainment" country="in" category="entertainment" />} />
-              <Route path="/general" element={<News pageSize={this.pageSize} key="general" country="in" category="general" />} />
-              <Route path="/health" element={<News pageSize={this.pageSize} key="health" country="in" category="health" />} />
-              <Route path="/science" element={<News pageSize={this.pageSize} key="science" country="in" category="science" />} />
-              <Route path="/sports" element={<News pageSize={this.pageSize} key="sports" country="in" category="sports" />} />
-              <Route path="/technology" element={<News pageSize={this.pageSize} key="technology" country="in" category="technology" />} />
+              <Route path="/" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="general" country={'in'} category={'general'}/>} />
+              <Route path="/business" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="business" country={'in'} category={'business'}/>} />
+              <Route path="/entertainment" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="entertainment" country="in" category="entertainment" />} />
+              <Route path="/general" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="general" country="in" category="general" />} />
+              <Route path="/health" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="health" country="in" category="health" />} />
+              <Route path="/science" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="science" country="in" category="science" />} />
+              <Route path="/sports" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="sports" country="in" category="sports" />} />
+              <Route path="/technology" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="technology" country="in" category="technology" />} />
             </Routes>
           </Router>
         </div>
       // <div>
       //   <NavBar />
-      //   <News pageSize={this.pageSize} country={'in'} category={'science'}/>
+      //   <News setProgress={this.setProgress}  pageSize={this.pageSize} country={'in'} category={'science'}/>
       // </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3 mt-4'>
           <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%' ,zIndex:1}}>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'80%' ,zIndex:1}}>
               {source}</span>
             <img src={imageUrl?imageUrl:"https://www.hindustantimes.com/ht-img/img/2024/05/27/1600x900/TOPSHOT-PNG-LANDSLIDE-RESCUE-0_1716842786732_1716842809283.jpg"} className="card-img-top" alt="..." />
             <div className="card-body">
